dc1bca7fdcf86f1ed702a6b4e77882c007a31a9f
eug0212 <43792940+eug0212@users.noreply.github.com>
2022-05-07 15:58:56 -0400

    added a bunch of updates, including finally app.py for the music quiz

--- final-project/app.py
+++ final-project/app.py
@@ -175,6 +175,44 @@ def musicians():
         return render_template("musicians.html", mus_list=mus_list)
 
 
+quiz_host = os.environ.get('FLASK_HOST')
+quiz_port = os.environ.get('FLASK_PORT')
+
+questions = [
+    {
+        "id": "1",
+        "question": "What is the Capital of Syria?",
+        "answers": ["a) Beirut", "b) Damascus", "c) Baghdad"],
+        "correct": "b) Damascus"
+    },
+    {
+        "id": "2",
+        "question": "What is the square root of Pi?",
+        "answers": ["a) 1.7724", "b) 1.6487", "c) 1.7872"],
+        "correct": "a) 1.7724"
+    },
+    {
+        "id": "3",
+        "question": "How many counties are there in England?",
+        "answers": ["a) 52", "b) 48", "c) 45"],
+        "correct": "b) 48"
+    }
+]
+
+
+@app.route("/musicquiz", methods=['POST', 'GET'])
+@login_required
+def musicquiz():
+    if request.method == 'GET':
+        return render_template("index.html", data=questions)
+    else:
+        result = 0
+        total = 0
+        for question in questions:
+            if request.form[question.get('id')] == question.get('correct'):
+                result += 1
+            total += 1
+        return render_template('results.html', total=total, result=result)
 
 @app.route("/musicquiz")
 @login_required
@@ -183,3 +221,7 @@ def musicquiz():
 
     mus_list = db.execute("SELECT * FROM users")
     return render_template("musicquiz.html", mus_list=mus_list)
+
+
+if __name__ == "__main__":
+    app.run(host=quiz_host, port=quiz_port)
